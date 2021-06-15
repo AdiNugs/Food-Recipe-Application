c4dc534f4dd2dd58b84072ac321220ee2034a449
Adi Nugraha <adin880@gmail.com>
2021-06-15 18:27:12 +0700

    Added Register

--- app/src/main/java/com/example/sumfood/MainActivity.java
+++ app/src/main/java/com/example/sumfood/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.sumfood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etEmail, etPassword;
+    Button btnLogin, btnLoginRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLoginRegister = findViewById(R.id.btnLoginRegister);
+
+        btnLoginRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, register.class);
+                startActivity(intent);
+                
+            }
+        });
+
+
     }
 }
